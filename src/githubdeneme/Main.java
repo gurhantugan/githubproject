cb6d132482e88f2e2ca294a7c6cd64792af34640
gurhantugan <95536059+gurhantugan@users.noreply.github.com>
2022-02-19 08:18:46 +0000

    my update

--- src/githubdeneme/Main.java
+++ src/githubdeneme/Main.java
@@ -1,5 +1,15 @@
 package githubdeneme;
 
+
 public class Main {
+    public static void main(String[]args){
+
+        System.out.println("Gurhan");
+        System.out.println("Tugan");
+        System.out.println("Denizli");
+        System.out.println("1975");
+
+    }
+
 
 }
