49ffdcaa710522d251a0bc1108b286229459e519
gurhantugan <95536059+gurhantugan@users.noreply.github.com>
2022-02-19 10:53:03 +0000

    Update Class01.java

--- src/githubdeneme/Class01.java
+++ src/githubdeneme/Class01.java
@@ -5,9 +5,13 @@ public class Class01 {
     public static void main(String[]args){
 
         System.out.println("Gurhan");
+        
         System.out.println("Tugan");
+        
         System.out.println("Denizli");
+        
         System.out.println("1975");
+        
         System.out.println("Duhok");
         System.out.println("Kurdistan ");
         System.out.println("Erbil Ishik College");
